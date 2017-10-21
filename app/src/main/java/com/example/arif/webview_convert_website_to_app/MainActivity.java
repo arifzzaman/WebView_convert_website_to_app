d78b2138058abad525c2bb1b156a17d406046144
arif <arif.glink@gmail.com>
2017-10-21 12:10:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/arif/webview_convert_website_to_app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.arif.webview_convert_website_to_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewID);
+        WebSettings webSettings = webView.getSettings();  // geting setting
+        webSettings.setJavaScriptEnabled(true); // enabling js from settings
+
+        webView.setWebViewClient(new WebViewClient());
+//        open every link in this app. dont go to the brower
+
+        webView.loadUrl("http://www.codefixit.com");
+    }
+}
